ee36733cb181fbe9f9008066e78be3e4af4b6a43
38128011 <38128011@L815WD220005.edu.cbe.ab.ca>
2025-11-20 10:20:09 -0700

    10:20

---
+++ Chapter8/src/Skillbuilders/Puck.java
@@ -0,0 +1,19 @@
+package Skillbuilders;
+
+public class Puck extends Disk
+{
+
+	private double weight;
+	private boolean standard;
+	private boolean youth;
+	
+	public Puck(double r, double t)
+	{
+		super(r, t);
+		
+	}
+	
+
+	
+
+}
