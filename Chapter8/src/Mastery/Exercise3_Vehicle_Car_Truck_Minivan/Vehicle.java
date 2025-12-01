058bd05d616bb1372166b55b566a72739d8ae413
38128011 <38128011@L815WD220005.edu.cbe.ab.ca>
2025-12-01 11:56:32 -0700

    11:56

--- Chapter8/src/Mastery/Exercise3_Vehicle_Car_Truck_Minivan/Vehicle.java
+++ Chapter8/src/Mastery/Exercise3_Vehicle_Car_Truck_Minivan/Vehicle.java
@@ -1,3 +1,17 @@
+/*
+Program: Vehicle.java          Last Date of this Revision: Dec. 1, 2025
+
+Purpose: Create a Vehicle class that is an abstract class defining the general details and actions associated with
+		a vehicle. Create Car, Truck, and Minivan classes that inherit the Vehicle class. The Car, Truck, and
+		Minivan classes should include additional members specific to the type of vehicle being represented.
+		Create client code to test the classes.
+
+Author: Vyom Patel 
+School: CHHS
+Course: Computer Programming 30
+
+ */
+
 package Mastery.Exercise3_Vehicle_Car_Truck_Minivan;
 
 public abstract class Vehicle
