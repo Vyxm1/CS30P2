2737a20d50083be9d45be209fdc455948498d78a
38128011 <38128011@L815WD220005.edu.cbe.ab.ca>
2025-12-09 09:06:39 -0700

    9:06

--- Chapter12/src/Skillbuilders/SearchLocations/Searches.java
+++ Chapter12/src/Skillbuilders/SearchLocations/Searches.java
@@ -1,14 +1,16 @@
-package Mastery;
+package Skillbuilders.SearchLocations;
 
-public class TernarySearch
-{
+
+
+ public class Searches
+ {
 	/**
-	 * Searches items array for goal.
+	 * Searches items array for goal
 	 * pre: items is sorted from low to high
 	 * post: Position of goal has been returned, or -1 has been returned if goal not found.
 	 */
-	public static int binarySearch(int[] items, int start, int end, int goal)
-	{
+	public static int binarySearch(int[] items, int start, int end, int goal) {
+
 		if (start > end)
 		{
 			return(-1);
@@ -16,32 +18,21 @@ public class TernarySearch
 		else
 		{
 			int mid = (start + end) / 2;
-
 			if (goal == items[mid])
 			{
 				System.out.println("Examining " + mid);
-				return mid;
+				return(mid);
 			}
 			else if (goal < items[mid])
 			{
 				System.out.println("Examining " + mid);
-				return binarySearch(items, start, mid - 1, goal);
+				return(binarySearch(items, start, mid-1, goal));
 			}
 			else
 			{
 				System.out.println("Examining " + mid);
-				return binarySearch(items, mid + 1, end, goal);
+				return(binarySearch(items, mid+1, end, goal));
 			}
-		}
-	}
-	
-	
-	
-	public static int ternarySearch(int[] items, int start, int end, int goal)
-	{
-		if (start > end)
-		{
-			return(-1);
 		}
 	}
 }
